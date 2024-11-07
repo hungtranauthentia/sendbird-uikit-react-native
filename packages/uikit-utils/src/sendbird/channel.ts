d6af12f7f8b2bbe5ca574fe84b82fb413fe25830
hungtranauthentia <hung@authentia.ca>
2024-11-07 17:17:38 +0700

    Disable input from custom TIS workflows

--- packages/uikit-utils/src/sendbird/channel.ts
+++ packages/uikit-utils/src/sendbird/channel.ts
@@ -22,7 +22,9 @@ export const getGroupChannelChatAvailableState = (channel: SendbirdGroupChannel)
   const isOperator = channel.myRole === 'operator';
   const frozen = channel.isFrozen && !isOperator;
   const muted = channel.myMutedState === 'muted';
-  const disabledByWorkflow = channel.lastMessage?.extendedMessagePayload?.disable_chat_input === true;
+  const disabledByWorkflow =
+    channel.lastMessage?.extendedMessagePayload?.disable_chat_input === true ||
+    channel.lastMessage?.data.includes('disableChatInput') === true;
   const disabled = frozen || muted || disabledByWorkflow;
   return { disabled, frozen, muted, disabledByWorkflow };
 };
